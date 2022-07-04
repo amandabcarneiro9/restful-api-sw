663da5e4f4f7e1320455e97df9ba8382d0194a48
Amanda <amandabcarneiro9@gmail.com>
2022-07-04 08:44:03 +0200

    sorting by height and order by asc and desc

--- repositories/charactersRepository.js
+++ repositories/charactersRepository.js
@@ -2,14 +2,12 @@ const films = require("../data/films.json");
 const people = require("../data/people.json");
 
 module.exports = {
-  async getCharactersFromMovie(searchTerm, gender) {
+  async getCharactersFromMovie(searchTerm, gender, sortBy, orderBy) {
     const filmsUrl = films
       .filter((film) => {
         return film.title.toLowerCase().includes(searchTerm.toLowerCase());
       })
-      .map((film) => {
-        return film.url;
-      });
+      .map((film) => film.url);
 
     const filmIsIncluded = (filmUrl) => filmsUrl.includes(filmUrl);
 
@@ -20,6 +18,22 @@ module.exports = {
     });
     const results = peopleFound;
 
+    if (sortBy) {
+      const sortFunction = decideSortFunctionByHeight(sortBy, orderBy);
+      results.sort(sortFunction);
+    }
+
     return results;
   },
 };
+
+const decideSortFunctionByHeight = (sortBy, orderBy) => {
+  if (sortBy === "height" && orderBy === "asc") return sortByHeightAscending;
+  if (sortBy === "height" && orderBy === "desc") return sortByHeightDescending;
+};
+
+const sortByHeightAscending = (person1, person2) =>
+  parseInt(person1.height) - parseInt(person2.height);
+
+const sortByHeightDescending = (person1, person2) =>
+  sortByHeightAscending(person2, person1);
--- routes/movies.js
+++ routes/movies.js
@@ -9,21 +9,35 @@ const POSSIBLE_GENDER = ["male", "female", "hermaphrodite", "n/a", "none"];
 router.get("/characters", async (req, res) => {
   const searchTerm = req.query.searchTerm ? req.query.searchTerm.trim() : "";
   const gender = req.query.gender ? req.query.gender.trim() : "";
+  // const sortBy = req.query.sortBy ? req.query.sortBy.trim() : "";
+  // const orderBy = req.query.orderBy ? req.query.orderBy.trim() : "";
 
   //validate the gender on request
-  const errors = [
-    gender &&
-      !POSSIBLE_GENDER.includes(gender) &&
-      `The gender ${gender} is not valid.`,
-  ].filter(Boolean);
+  const errors = [validateGender(gender)].filter(Boolean);
 
   if (errors.length > 0) {
     res.status(400).send({ errors });
     return;
   }
 
-  const result = await getCharactersFromMovie(searchTerm, gender);
+  //validate the sortBy and orderBy
+
+  // if(sortBy === "height"){
+  //   return
+  // }
+
+  const result = await getCharactersFromMovie(
+    searchTerm,
+    gender,
+    sortBy,
+    orderBy
+  );
   res.send(result);
 });
+
+const validateGender = (gender) =>
+  gender &&
+  !POSSIBLE_GENDER.includes(gender) &&
+  `The gender ${gender} is not valid.`;
 
 module.exports = router;
